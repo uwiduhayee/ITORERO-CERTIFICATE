c0c028c4c647bb8ba51b18ac6b24c5f69764203c
NSHIMIYIMANA Dieudonne <59695767+Dieudonnenob@users.noreply.github.com>
2022-05-23 21:44:31 +0200

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+unset($_SESSION['username']);
+	unset($_SESSION['Rabemployee_id']);
+	unset($_SESSION['username']);
+	unset($_SESSION['password']);
+	unset($_SESSION['username']);
+	?>
+<meta HTTP-EQUIV="Refresh" CONTENT="0; URL=index.php">
